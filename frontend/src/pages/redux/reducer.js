250e63de96b2c9d87a6483be67e03461074ba4f5
Cristian Arias <javier.0813@hotmail.com>
2023-12-14 16:09:05 -0500

    Se deja la carpeta Back

--- frontend/src/pages/redux/reducer.js
+++
@@ -1,9 +0,0 @@
-
-import { combineReducers } from 'redux';
-import cartReducer from './cartreducer';
-
-const rootReducer = combineReducers({
-    cart: cartReducer,
-});
-
-export default rootReducer;
